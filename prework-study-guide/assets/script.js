3995ed10c6fabb4d0f2de609d808a308c0e2337f
Sanjeev <sanjeevthapa961@gmail.com>
2024-05-25 04:01:55 +1000

    adding a ajavscript file

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,9 @@
+const topic="HTML";
+if (topic=="HTML") {console.log("learn html");}
+else if (topic=="CSS"){console.log("lets code CSS");}
+else if (topic=="Git"){console.log("lets study git");}
+else if (topic=="javascript"){console.log("lets code javascript");}
+else
+{console.log("please try again");}
+const topics=["HTML","CSS","git","javascript"];
+for(let x=0;x<topics.length;x++){console.log(topics[x]);}
